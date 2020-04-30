59d42808a1519d5979185331e9b1c84f99bc35aa
loxt <emannuelloxt@hotmail.com>
2020-04-30 17:26:16 -0400

    chore: WIP setup initial

---
+++ backend/src/models/Article.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const ArticleSchema = new mongoose.Schema({
+  text: String,
+  title: String,
+  description: String,
+  feature_img: String,
+  teste: String,
+  author: {
+    type: mongoose.schema.ObjectId,
+    ref: 'User',
+  },
+  comments: [
+    {
+      author: {
+        type: mongoose.schema.ObjectId,
+        ref: 'User',
+      },
+      text: String,
+    },
+  ],
+});
+
+ArticleSchema.methods.clap = function clap() {
+  this.claps += 1;
+  return this.save();
+};
+
+ArticleSchema.methods.comment = function comment(c) {
+  this.comments.push(c);
+  return this.save();
+};
+
+ArticleSchema.methods.addAuthor = function addAuthor(authorId) {
+  this.author = authorId;
+  return this.save();
+};
+
+ArticleSchema.methods.getUserArticle = function getUserArticle(id) {
+  ArticleSchema.find({ author: id }).then((article) => {
+    return article;
+  });
+};
+
+module.exports = mongoose.model('Article', ArticleSchema);
